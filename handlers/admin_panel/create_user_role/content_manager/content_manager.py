50df56d2d3e212565fe46ed49b81aca784856e83
Artyom Uschenko <net775@yandex.ru>
2024-12-02 17:57:03 +0300

    Реализовано проверка введенного ID пользователя и администратора

--- handlers/admin_panel/create_user_role/content_manager/content_manager.py
+++ handlers/admin_panel/create_user_role/content_manager/content_manager.py
@@ -15,13 +15,34 @@ class FSM_create_user_role_content_manager(StatesGroup):
 async def load_user_role_admin(callback_query: types.Message):
     await FSM_create_user_role_content_manager.user_id.set()
     await bot.delete_message(callback_query.from_user.id, message_id=callback_query.message.message_id)
-    await bot.send_message(callback_query.from_user.id, "Роль была успешно добавлена \n Введите id пользователя")
+    await bot.send_message(callback_query.from_user.id, "Роль  успешно выбрана \n Введите id пользователя")
 
 @dp.message_handler(state=FSM_create_user_role_content_manager.user_id)
 async def load_user_id(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['user_id'] = message.text
+        str_user_id = data["user_id"]
 
-    await state.finish()
-    await bot.delete_message(message.from_user.id, message_id=message.message_id)
-    await bot.send_message(message.from_user.id, F"Контент менеджер  с ID {data['user_id']} успешно создан", reply_markup=admin_panel_keyboard_back_to_main_menu)
+        try:
+            int_user_id = int(str_user_id)
+
+            if int_user_id < 0:
+                await state.finish()
+                await bot.send_message(message.from_user.id,
+                                       f"Введите корректный id пользователя \n Вы вели следующий ID: {str_user_id}",
+                                       reply_markup=admin_panel_keyboard_back_to_main_menu)
+
+            else:
+                await state.finish()
+                await bot.send_message(message.from_user.id, f"Контент менеджер с ID {data['user_id']} успешно создан",
+                                       reply_markup=admin_panel_keyboard_back_to_main_menu)
+
+        except ValueError:
+            await state.finish()
+            await bot.send_message(message.from_user.id,
+                                   f"ID должен содержать только цифры \n Вы вели следующий ID: {str_user_id}",
+                                   reply_markup=admin_panel_keyboard_back_to_main_menu)
+
+    # await state.finish()
+    # await bot.delete_message(message.from_user.id, message_id=message.message_id)
+    # await bot.send_message(message.from_user.id, F"Контент менеджер  с ID {data['user_id']} успешно создан", reply_markup=admin_panel_keyboard_back_to_main_menu)
